7a340ab6083a78c5a8bb02bf1411e2bde4d8e121
moni10jmj@gmail.com <praveen@786>
2017-12-13 19:42:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jmjpc/scooterman/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.jmjpc.scooterman;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
